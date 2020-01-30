93214b0a3da1d24febe5148f5353078ffd477b29
Oleksandr Otchenashev <alexandr.otchenashev@gmail.com>
2020-01-30 11:07:30 +0100

    Adding FosJsRouting bundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
 ];
